b35565d7e94eab06e5e789efefc04e270d4849c8
Ralph Ryan Peralta <ralphryan.peralta@gmail.com>
2024-06-14 15:22:26 +0000

    AppUser Identity fix

--- src/HowsYourDayAPI/Data/HowsYourDayAppDbContext.cs
+++ src/HowsYourDayAPI/Data/HowsYourDayAppDbContext.cs
@@ -1,11 +1,10 @@
 using HowsYourDayAPI.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HowsYourDayAPI.Data
 {
-    public class HowsYourDayAppDbContext: IdentityDbContext<IdentityUser>
+    public class HowsYourDayAppDbContext: IdentityDbContext<AppUser>
     {
         public HowsYourDayAppDbContext(DbContextOptions<HowsYourDayAppDbContext> options) : base(options)
         {
